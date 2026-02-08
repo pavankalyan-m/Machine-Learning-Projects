30834effad0ad78f37e7e3530efab3e60cc8d84b
Pavan Kalyan Reddy <69005522+pavankalyan-m@users.noreply.github.com>
2026-02-08 15:48:12 +0530

    Update prediction_maker.py

--- Health-Insurance-Premium-Calculator/app/prediction_maker.py
+++ Health-Insurance-Premium-Calculator/app/prediction_maker.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
+import os
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ARTIFACTS_DIR = os.path.join(BASE_DIR, "artifacts")
 
